89c805adb16c8f53f1f257ed9c93950d68c20cf7
Kishore Ravi <kishoreravi@Kishores-MacBook-Air.local>
2025-11-30 20:13:04 +0530

    Final Code RK-26

--- newcode.py
+++ newcode.py
@@ -1,27 +1,5 @@
-x=10
-y=5
-z=x+y
-print(z)
-
 print("Kishore")
-
-TEAM = "TIGER"
-print(TEAM)
-
-Power = ["Kishore","TREE","UNUSED"]
-print(Power)
-
-x = 10   
-
-def show():
-    print(x)  
-
-show()
-print(x)
-
-Time = "BAD"
-
-def show ()
-    print(show)
-show ()
-print(Time)
+x="Kishore"
+Y="raj"
+Z=x+Y
+print(Z)
